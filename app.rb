155fbc7f898e4aacaef619897e749589f80aa155
a-sac <asergioalvesc@gmail.com>
2025-02-17 18:01:06 +0000

    PoC version: more nutritional info, like and dislike, more logs

--- app.rb
+++ app.rb
@@ -1,42 +1,77 @@
 require 'sinatra'
 require 'openai'
 require 'dotenv'
+require 'json'
 
 Dotenv.load
 
 client     = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
 show_input = false
+meal_description = nil
 ingredients = []
-macros      = []
-recipe_name = nil
+meal_nutrition = {}
+feedback_submit = false
 
 get '/' do
-  erb :index, locals: { recipe_name: recipe_name, show_input: show_input, ingredients: ingredients, macros: macros }
+  erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
 end
 
 post '/toggle_div' do
   show_input = !show_input
 
-  erb :index, locals: { recipe_name: recipe_name, show_input: show_input, ingredients: ingredients, macros: macros }
+  erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
 end
 
 post '/remove' do
   ingredients = []
-  macros      = []
-  recipe_name = nil
+  meal_nutrition = []
+  meal_description = nil
+  feedback_submit = false
 
-  erb :index, locals: { recipe_name: recipe_name, show_input: show_input, ingredients: ingredients, macros: macros }
+  erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
+end
+
+post '/like' do
+  puts "| USER FEEDBACK BEGIN |"
+  puts "Ingredients: #{ingredients}"
+  puts "Nutritional info: #{meal_nutrition}"
+  puts "Description: #{meal_description}"
+  puts "Result: Approve"
+  puts "| USER FEEDBACK END |"
+
+  ingredients = []
+  meal_nutrition = []
+  meal_description = nil
+  feedback_submit = true
+
+  erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
+end
+
+post '/dislike' do
+  puts "| USER FEEDBACK BEGIN |"
+  puts "Ingredients: #{ingredients}"
+  puts "Nutritional info: #{meal_nutrition}"
+  puts "Description: #{meal_description}"
+  puts "Result: Disapprove"
+  puts "| USER FEEDBACK END |"
+
+  ingredients = []
+  meal_nutrition = []
+  meal_description = nil
+  feedback_submit = true
+
+  erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
 end
 
 post '/ask' do
-  recipe_name = params[:recipe_name]
-  show_input  = !show_input
+  meal_description = params[:meal_description]
+  show_input = !show_input
+  feedback_submit = false
 
-  if recipe_name.nil? || recipe_name.strip.empty?
-    @error = "Please enter a valid recipe name."
-
-    recipe_name = nil
-    return erb :index, { recipe_name: recipe_name, show_input: show_input, ingredients: ingredients, macros: macros }
+  if meal_description.nil? || meal_description.strip.empty?
+    @error = "Please enter a valid meal description."
+    meal_description = nil
+    return erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition }
   end
 
   begin
@@ -44,89 +79,83 @@ post '/ask' do
       parameters: {
         model: "gpt-4",
         messages: [
-          { 
-            role: 'user', 
+          {
+            role: 'user',
             content: <<~PROMPT
-              Give me a list of ingredients for a #{recipe_name} recipe. 
-              If this is not a recipe, don't provide anything.
-              For each ingredient, include the name of the food and a precise quantity. 
-              Avoid using vague terms like 'to taste,' 'as needed,' or similar. 
-              Each ingredient must have a clear and measurable quantity.
-              The quantity must be for only one person.
-              Use only grams for the quantity, don't give any other info, just grams.
-              To separate the name of the food and the quantity, use a "-"
-              The name of the food, should be the one used on the USDA DB.
-              Up to 7 ingredients. Only provide foods who are strictly necessary for the recipe.
-              It shall follow this structure: Food Name - 50g\nOther food name: 300 grams
-              Finally, provide the total number of energy in kcal, fat in g, Carbohydrate in g, and protein in g of this recipe.
-              It shall follow this structure: Energy: X kcal\nFat: X grams\nCarbohydrates: X grams\nProtein: X grams
-              To separate the name of the macros and the quantity, use a ":"
+              Given the following meal description, extract a list of ingredients and their nutritional values.
+              It's ok if the nutritional values of those ingredients are estimated but not exact.
+
+              Meal description: "#{meal_description}"
+
+              **Requirements:**
+              - Each ingredient must include:
+                - Name
+                - Quantity
+                - Energy (kcal)
+                - Fat (g)
+                - Carbohydrates (g)
+                - Protein (g)
+              - The response must be in **valid JSON** format.
+              - The total nutritional values (`meal_nutrition`) must match the sum of individual ingredient values.
+              - The portions shall be adjusted to one person.
+              - Avoid using vague terms like 'to taste,' 'as needed,' or similar. 
+              - Use only grams for the quantity, don't give any other info, just grams.
+              - The name of the food, should be the one used on the USDA DB or any other reliable source you have access to.
+              - Please, don't return text, the response must be only json and respect the format presented below.
+
+              **Response Format:**
+              ```json
+              {
+                "ingredients": [
+                  { "name": "Ingredient Name", "quantity": "150g", "energy": X, "fat": X, "carbohydrates": X, "protein": X },
+                  ...
+                ],
+                "meal_nutrition": { "energy": X, "fat": X, "carbohydrates": X, "protein": X }
+              }
+              ```
+
+              **Error Handling:**
+              - If you can't perform the action or there is any other error, return a **valid JSON** error response with the error description:
+              ```json
+              { error: "Unable to determine ingredients and nutrition for the given meal description." }
+              ```
+              - Do not return partial or incomplete data.
             PROMPT
           }
         ]
       }
     )
-    
+
     puts "Raw response: #{response}"
     info = response.dig('choices', 0, 'message', 'content')
 
     if info
-      ingredients, macros = process_info(info)
-    end
+      json_match = info.match(/```.*json\n(.*?)\n```/m)
+      json_string = json_match ? json_match[1] : info.strip
 
-    puts "Ingredients: #{ingredients}, Macros: #{macros}"
-    if ingredients.to_a.empty? && macros.to_a.empty?
-      recipe_name = nil
+      parsed_data = JSON.parse(json_string) rescue nil
+      puts "JSON: #{parsed_data}"
+
+      if parsed_data && parsed_data['ingredients'] && parsed_data['meal_nutrition']
+        ingredients = parsed_data['ingredients'].map do |ingredient|
+          ingredient.transform_keys(&:to_sym)
+        end
+        puts "Ingredients: #{ingredients}"
+
+        meal_nutrition = parsed_data['meal_nutrition'].transform_keys(&:to_sym)
+        puts "Nutritional Info: #{meal_nutrition}"
+      else
+        @error = "We were not able to process your request. Please try again."
+        show_input = true
+      end
+    else
+      @error = "No response received. Please try again."
       show_input = true
-
-      @error = "We were not able to process your request. Please try again."
     end
 
-    erb :index, locals: { recipe_name: recipe_name, show_input: show_input, ingredients: ingredients, macros: macros }
+    erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
   rescue OpenAI::Error => e
     @error = "An error occurred while contacting OpenAI: #{e.message}"
-    erb :index, locals: { recipe_name: recipe_name, show_input: show_input, ingredients: ingredients, macros: macros }
+    erb :index, locals: { meal_description: meal_description, show_input: show_input, ingredients: ingredients, meal_nutrition: meal_nutrition, feedback_submit: feedback_submit }
   end
-end
-
-def process_info(info)
-  meal_info = info.split("\n").reject(&:empty?)
-  macros = []
-
-  return [] if meal_info.length < 2
-
-  structured_ingredients = meal_info.map do |ingredient|
-    ingredient = ingredient.strip
-    ingredient = ingredient.sub(/^\d+\.\s*/, '')
-
-    parts = ingredient.split(" - ", 2)
-
-    if invalid_duo?(parts)
-      parts = ingredient.split(":", 2)
-      
-      next if invalid_duo?(parts)
-
-      macro_name = parts[0].strip
-      macro_quantity = parts[1].strip
-
-      macros << { quantity: macro_quantity, name: macro_name }
-
-      next
-    end
-    
-    name = parts[0].strip
-    quantity = parts[1].strip
-
-    if quantity.downcase.include?("to taste") || quantity.downcase.include?("as needed")
-      next
-    end
-
-    { quantity: quantity, name: name }
-  end
-  
-  [structured_ingredients.compact, macros.compact]
-end
-
-def invalid_duo?(parts)
-  parts.length < 2 || parts[0].empty? || parts[1].empty?
 end
